b2d9497d2a898e3d3a8499e9970aaae4c499e08b
Prabin Paudel <prabinpaudel43@gmail.com>
2023-04-20 06:55:44 +0545

    image path fixed

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ def object2Image():
 
    if request.method == "POST" and uploadedString:
       ObjectAndImage.objectToImage(uploadedString, imageName)
-      imageStoreString = os.path.join(os.getcwd(), f'static/{imageName}.png')
-      return render_template("img.html", user_image = imageStoreString)
+      return render_template("img.html", user_image = f'/static/{imageName}.png')
 
    return render_template("index.html")
 
